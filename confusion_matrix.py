958ed19e8e10432899f5999adb5638c36eaa22b4
tomoyaf <gazimum@gmail.com>
2017-10-17 22:49:25 +0900

    first commit

---
+++ confusion_matrix.py
@@ -0,0 +1,33 @@
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+
+def plot_confusion_matrix(y_true, y_pred, title = "Confusion matrix",
+                          cmap = plt.cm.Blues, save_flg = False):
+    classes = [str(i) for i in range(10)]
+    labels = range(10)
+
+    cm = confusion_matrix(y_true, y_pred, labels=labels)
+    plt.figure(figsize=(14, 12))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title, fontsize=40)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, fontsize=20)
+    plt.yticks(tick_marks, classes, fontsize=20)
+
+    print('Confusion matrix, without normalization')
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label', fontsize=30)
+    plt.xlabel('Predicted label', fontsize=30)
+    plt.show()
+
+    if save_flg:
+        plt.savefig("./confusion_matrix.png")
